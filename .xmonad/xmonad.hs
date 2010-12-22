53757f7b67615dec906789063971c035213cac85
Aaron Adelson <aaronsadelson@gmail.com>
2010-12-21 18:41:06 -0800

    added X config files, .gitignore

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -1,14 +1,16 @@
 import XMonad
 import XMonad.Util.EZConfig
---import XMonad.Util.EZConfig
---import XMonad.Config.Gnome
+import XMonad.Config.Gnome
+import Control.Arrow
+import Data.Bits
+import qualified Data.Map as M
+import Data.Monoid
 
-main = xmonad $ defaultConfig 
-        { terminal = "terminator"
-        , modMask = mod4Mask -- set the mod key to the windows key
-        }
-        `additionalKeysP` 
-                 [ ("M-m", spawn "echo")
-                 , ("M-<Backspace>", withFocused hide) -- N.B. this is an absurd thing to do
-		         , ("M-S-q", spawn "gnome-session-save --gui --logout-dialog")
-                 ]
+main = xmonad $ defaultConfig
+        { terminal      = "terminator"
+        , modMask       = mod4Mask -- set the mod key to the windows key
+        , workspaces  = ["1","2","3","4","5","6","7","8","9"]
+        } `additionalKeysP` myKeys
+
+myKeys =
+       [("M-z", spawn "gnome-session-save --gui --logout-dialog")]
