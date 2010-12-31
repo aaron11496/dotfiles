b34ebc498ba948c3fe3ca815a321a4d5cbc5f52a
aaron11496 <aaronsadelson@gmail.com>
2010-12-30 21:09:22 -0800

    added xmobar, xsession

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -1,18 +1,50 @@
 import XMonad
+import XMonad.Hooks.DynamicLog
 import XMonad.Util.EZConfig
 import XMonad.Config.Gnome
-import Control.Arrow
 import Data.Bits
 import qualified Data.Map as M
+import qualified XMonad.StackSet as W
+
 import Data.Monoid
 
-myKeys =
-       [("M-z", spawn "gnome-session-save --gui --logout-dialog")]
+rb_pause = "rhythmbox-client --no-present --no-start --play-pause"
+rb_next  = "rhythmbox-client --no-present --no-start --next"
+rb_prev = "rhythmbox-client --no-present --no-start --previous"
 
+myKeys conf@(XConfig {XMonad.modMask = modMask}) = M.fromList $
+    [ ((modMask .|. shiftMask, xK_z), spawn "gnome-session-save --gui --logout-dialog")
+    , ((modMask, xK_x), spawn rb_pause)
+    ]
 
-main = xmonad $ defaultConfig
-        { terminal      = "urxvt"
-        , modMask       = mod4Mask -- set the mod key to the windows key
-        , workspaces  = ["1","2","3","4","5","6","7","8","9"]
-        } `additionalKeysP` myKeys
+myWorkspaces = [ "1-base", "2-mail", "3-chat", "4-term", "5-code" , "6-music"
+               , "7", "8", "9" ]
 
+-- launch certain programs only on certain workspaces
+myManageHook = composeAll [ className =? "Pidgin"    --> doF (W.shift "3-chat")
+                          , className =? "Skype"     --> doF (W.shift "3-chat")
+                          , className =? "Rhythmbox" --> doF (W.shift "6-music")
+                          ]
+
+myConfig = defaultConfig
+           { terminal    = "urxvt"
+           , modMask     = mod4Mask -- set the mod key to the windows key
+           , workspaces  = myWorkspaces
+           --, keys        = myKeys
+           , manageHook  = myManageHook <+> manageHook defaultConfig
+           }
+
+myPP = xmobarPP { ppCurrent = xmobarColor "#00A000" ""
+                , ppVisible = xmobarColor "#C0C0C0" ""
+                , ppHidden = xmobarColor "#909090" ""
+                , ppHiddenNoWindows = xmobarColor "#606060" ""
+                , ppUrgent = xmobarColor "orange" ""
+                , ppSep = "  "
+                , ppWsSep = " "
+                , ppTitle = xmobarColor "#00A000" ""
+                }
+
+-- toggle xmobar on and off with mod-b
+toggleStatusbarKey XConfig {XMonad.modMask = modMask} = (modMask, xK_b)
+
+main = xmonad =<< statusBar "xmobar" myPP toggleStatusbarKey myConfig
