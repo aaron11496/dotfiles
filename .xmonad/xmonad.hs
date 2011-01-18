fecee1dc99dfea581e97e7ea895f803758c8cb5a
Aaron Adelson <aaronsadelson@gmail.com>
2011-01-17 23:17:57 -0800

    Volume control keyboard shortcuts for xmonad, formatting

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -3,39 +3,41 @@ import qualified Data.Map as M
 import XMonad
 import XMonad.Hooks.DynamicLog
 import XMonad.Hooks.ManageDocks
+import XMonad.Hooks.ManageHelpers
 import XMonad.Layout.NoBorders
 import XMonad.Util.Run(spawnPipe)
 import qualified XMonad.StackSet as W
 
 
--- workspaces names
 myWorkspaces =
     [ "1-base", "2-mail", "3-chat", "4-term", "5-code" , "6-music"
     , "7", "8", "9" ]
 
--- keyboard shortcuts
-rb_pause = "rhythmbox-client --no-present --play-pause"
-rb_next  = "rhythmbox-client --no-present --next"
-rb_prev  = "rhythmbox-client --no-present --previous"
+myTerminal = "urxvt"
+
+-- set mod to super (default is left-alt)
+myModMask = mod4Mask
 
 myKeys x =
-    [ ((modMask x, xK_x), spawn rb_pause)
-    , ((modMask x, xK_z), spawn rb_prev)
-    , ((modMask x, xK_c), spawn rb_next)
+    [ --((modMask x, xK_b), sendMessage ToggleStruts) -- doesn't work right :(
+      ((modMask x, xK_equal), spawn "amixer set 'Master' 5%+")
+    , ((modMask x, xK_minus), spawn "amixer set 'Master' 5%-")
+    , ((modMask x, xK_x), spawn "rhythmbox-client --no-present --play-pause")
+    , ((modMask x, xK_z), spawn "rhythmbox-client --no-present --previous")
+    , ((modMask x, xK_c), spawn "rhythmbox-client --no-present --next")
     ]
-
 myKeyMap x = M.union (keys defaultConfig x) (M.fromList (myKeys x))
 
---
 myLayoutHook = avoidStruts $ smartBorders $ layoutHook defaultConfig
 
--- launch certain programs only on certain workspaces
 myManageHook =
     composeAll
-    [ className =? "Pidgin"    --> doF (W.shift "3-chat")
+    [ isFullscreen --> doFullFloat -- make full-screen windows work
+      -- launch certain programs only on certain workspaces
+    , className =? "Pidgin"    --> doF (W.shift "3-chat")
     , className =? "Skype"     --> doF (W.shift "3-chat")
     , className =? "Rhythmbox" --> doF (W.shift "6-music")
-    ]
+    ] <+> manageHook defaultConfig
 
 -- logging for xmobar to use
 myLogHook h = dynamicLogWithPP $ myPP { ppOutput = hPutStrLn h }
@@ -45,31 +47,28 @@ myPP =
     xmobarPP
     { ppCurrent = xmobarColor "#00B000" ""
     , ppVisible = xmobarColor "#B0B000" ""
-    , ppHidden = xmobarColor "#B0B0B0" ""
+    , ppHidden  = xmobarColor "#B0B0B0" ""
     , ppHiddenNoWindows = xmobarColor "#606060" ""
-    , ppUrgent = xmobarColor "orange" ""
-    , ppSep = "   "
-    , ppWsSep = " "
+    , ppUrgent  = xmobarColor "orange" ""
+    , ppSep     = "   "
+    , ppWsSep   = " "
       -- This centers the window title, but fails when there's no window
-    , ppTitle = wrap "}" "{" . xmobarColor "#00A000" ""
+    , ppTitle   = wrap "}" "{" . xmobarColor "#00A000" ""
     }
-
--- set mod to windows-key (default is left-alt)
-myModMask = mod4Mask
-
 
 main = do
   xmproc <- spawnPipe "xmobar"  -- start xmobar
   xmonad $ defaultConfig
-             { manageHook = myManageHook <+> manageHook defaultConfig
+             { manageHook = myManageHook
              , layoutHook = myLayoutHook
 	     --, borderWidth = myBorderWidth
 	     --, normalBorderColor = myNormalBorderColor
-	     --, focusedBorderColor = myFocusedBorderColor
-	     , keys = myKeyMap
-	     , logHook = myLogHook xmproc
+	     --, focusedBorderColor = myFocusedBorderColo
+             , terminal = myTerminal
              , modMask = myModMask
-             , terminal = "urxvt"
+             , keys = myKeyMap
 	     , workspaces = myWorkspaces
+             , logHook = myLogHook xmproc
+
              --, focusFollowsMouse = False
 	     }
