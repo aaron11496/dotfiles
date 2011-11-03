4e959a12e20746aa13dff7bd821dbbd98f1f5d85
Aaron Adelson <aaronsadelson@gmail.com>
2011-11-03 11:14:47 -0700

    New XMonad config using Gnome for laptop

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -1,11 +1,5 @@
--- http://xmonad.org/xmonad-docs/xmonad/index.html
--- http://xmonad.org/xmonad-docs/xmonad-contrib/index.html
--- http://hackage.haskell.org/packages/archive/X11/1.5.0.0/doc/html/Graphics-X11-Types.html#t:KeySym
-import System.IO
-import qualified Data.Map as M
-
 import XMonad
-import XMonad.Hooks.DynamicLog
+import XMonad.Config.Gnome
 import XMonad.Hooks.ManageDocks
 import XMonad.Hooks.ManageHelpers
 import XMonad.Layout.Grid
@@ -13,7 +7,6 @@ import XMonad.Layout.IM
 import XMonad.Layout.NoBorders
 import XMonad.Layout.PerWorkspace
 import XMonad.Layout.Reflect
-import XMonad.Util.Run(spawnPipe)
 import qualified XMonad.StackSet as W
 
 
@@ -22,69 +15,28 @@ myWorkspaces =
 
 myTerminal = "urxvt"
 
-myModMask = mod4Mask -- set mod to super (default is left-alt)
-
-myKeys x =
-    [ ((modMask x .|. shiftMask, xK_s), spawn "gnome-screensaver-command -l")
-    , ((modMask x, xK_bracketright), spawn "amixer set 'Master' 5%+")
-    , ((modMask x, xK_bracketleft), spawn "amixer set 'Master' 5%-")
-    , ((modMask x, xK_0), spawn "rhythmbox-client --no-present --previous")
-    , ((modMask x, xK_minus), spawn "rhythmbox-client --no-present --play-pause")
-    , ((modMask x, xK_equal), spawn "rhythmbox-client --no-present --next")
-    ]
-myKeyMap x = M.union (keys defaultConfig x) (M.fromList (myKeys x))
-
 myLayoutHook = avoidStruts
                $ smartBorders
                -- $ onWorkspace "draw" gimpLayout
                $ onWorkspace "chat" pidginLayout
                $ (Tall 1 (3/100) (1/2) ||| Full)
     where
-      -- gimpLayout   = withIM (0.11) (Role "gimp-toolbox")
-      --                $ reflectHoriz
-      --                $ withIM (0.15) (Role "gimp-dock") Full
       pidginLayout = reflectHoriz
                      $ withIM (0.15) (Role "buddy_list") Grid
 
 myManageHook =
     composeAll
-    [ isFullscreen --> doFullFloat -- make full-screen windows work
+    [ manageHook gnomeConfig
+    , isFullscreen --> doFullFloat -- make full-screen windows work
       -- launch certain programs only on certain workspaces
     , className =? "Pidgin"    --> doF (W.shift "chat")
     , className =? "Skype"     --> doF (W.shift "chat")
     , className =? "Rhythmbox" --> doF (W.shift "song")
-    --, className =? "Gimp"      --> doF (W.shift "draw")
-    ] <+> manageHook defaultConfig
+    ]
 
--- logging for xmobar to use
-myLogHook h = dynamicLogWithPP $ myPP { ppOutput = hPutStrLn h }
-
--- xmobar styling
-myPP =
-    xmobarPP
-    { ppCurrent         = xmobarColor "#00B000" ""
-    , ppVisible         = xmobarColor "#B0B000" ""
-    , ppHidden          = xmobarColor "#B0B0B0" ""
-    , ppHiddenNoWindows = xmobarColor "#606060" ""
-    , ppUrgent          = xmobarColor "orange" ""
-    , ppSep             = "   "
-    , ppWsSep           = "  "
-      -- This centers the window title, but fails when there's no window
-    , ppTitle           = wrap "}" "{" . xmobarColor "#00A000" ""
-    }
-
-main = do
-  xmproc <- spawnPipe "xmobar"  -- start xmobar
-  xmonad $ defaultConfig
-             { manageHook = myManageHook
-             , layoutHook = myLayoutHook
-	     --, borderWidth = myBorderWidth
-	     --, normalBorderColor = myNormalBorderColor
-	     --, focusedBorderColor = myFocusedBorderColor
-             , terminal   = myTerminal
-             --, modMask    = myModMask
-             , keys       = myKeyMap
-	     , workspaces = myWorkspaces
-             , logHook    = myLogHook xmproc
-             --, focusFollowsMouse = False
-	     }
+main = xmonad $ gnomeConfig
+       { manageHook = myManageHook
+       , layoutHook = myLayoutHook
+       , terminal   = myTerminal
+       , workspaces = myWorkspaces
+       }
