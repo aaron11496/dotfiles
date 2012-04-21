7018c95ae4abe9f4818f1a32f23090735d2c144d
Aaron Adelson <aaronsadelson@gmail.com>
2012-04-20 17:16:07 -0700

    Fixed .emacs.d install section

--- install.sh
+++ install.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-# Run from ~
+
+pushd ~
 
 FILENAMES=(
     '.bash_aliases'
     '.bashrc'
     '.csshrc'
     '.emacs'
-    '.emacs.d'
     '.gitconfig'
     '.gitignore'
     '.profile'
@@ -27,3 +27,22 @@ ln -s -v -b --suffix=".bak" ~/config/ipython_config.py ~/.ipython/profile_defaul
 for fn in config/venvwrapperhooks/*; do
     ln -s -v -b --suffix=".bak" ~/$fn ~/.virtualenvs/ ;
 done;
+
+if [[ ! -d ~/.emacs.d ]]; then
+    mkdirs ~/.emacs.d
+    for fn in .emacs.d/*; do
+        ln -s -v -b --suffix=".bak" ~/.emacs.d/ ;
+    done
+fi
+if [[ ! -d ~/.virtualenvs ]];
+    mkdir ~/.virtualenvs ;
+    for fn in config/venvwrapperhooks/*; do
+        ln -s -v -b --suffix=".bak" ~/$fn ~/.virtualenvs/ ;
+    done;
+
+if [[ ! -d ~/.emacs.d ]]; then
+    mkdir ~/.emacs.d ;
+    for fn in ~/config/.emacs.d/*; do
+        ln -s -v -b --suffix=".bak" $fn ~/.emacs.d/  ;
+    done
+fi
